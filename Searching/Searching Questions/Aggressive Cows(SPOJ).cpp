a9a40bd835f213569bd290bc6947248d4898aa86
rishav1808 <rishavraj1808@gmail.com>
2021-10-12 23:47:02 +0530

    Aggressive Cows

---
+++ Searching/Searching Questions/Aggressive Cows(SPOJ).cpp
@@ -0,0 +1,86 @@
+/*Aggresive Cows*/
+/* Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1,...,xN (0 <= xi <= 1,000,000,000).
+
+His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+
+Input
+t – the number of test cases, then t test cases follows.
+* Line 1: Two space-separated integers: N and C
+* Lines 2..N+1: Line i+1 contains an integer stall location, xi
+
+Output
+For each test case output one integer: the largest minimum distance. 
+Example
+Input:
+
+1
+5 3
+1
+2
+8
+4
+9
+Output:
+
+3
+Output details:
+
+FJ can put his 3 cows in the stalls at positions 1, 4 and 8,
+resulting in a minimum distance of 3.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+bool isPossible(int *a,int n,int cows,int mid)
+{
+    int count=1,dist=a[0];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]-dist>=mid)
+        {
+            count++;
+            dist=a[i];
+        }
+         if(count==cows)
+        return true;
+    }
+    return false;
+   
+}
+int aggresiveCows(int *a,int n,int cows)
+{
+    sort(a,a+n);
+    int start=1,end=a[n-1]-a[0];
+    int ans;
+    while(start<=end)
+    {
+        int mid=start+(end-start)/2;
+        if(isPossible(a,n,cows,mid)==true)
+        {
+            ans=mid;
+            start=mid+1;
+        }
+        else if(isPossible(a,n,cows,mid)==false)
+        {
+            end=mid-1;
+        }
+    }
+    return ans;
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,c;
+		cin>>n>>c;
+		int a[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		int ans=aggresiveCows(a,n,c);
+		cout<<ans<<endl;
+		
+	}
+	return 0;
+}
