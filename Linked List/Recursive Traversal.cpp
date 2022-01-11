e5e52f7811993b4511bd536d1f16716db999c861
rishav1808 <rishavraj1808@gmail.com>
2022-01-11 20:15:05 +0530

    Added Recursive Traversal

---
+++ Linked List/Recursive Traversal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void traversal(node *head)
+{
+    if(head==NULL)
+    return;
+    cout<<head->data<<" ";
+    traversal(head->next);
+}
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    traversal(head);
+    
+
+    return 0;
+}
+
