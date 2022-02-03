3592e5ef691da92f5e4194ea311c604a2314d333
rishav1808 <rishavraj1808@gmail.com>
2022-02-03 10:47:02 +0530

    Added Vector Implementation of Stack

---
+++ Stack/Vector Implementation of Stack.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+struct Stack{
+    vector<int>v;
+    
+    void push(int x)
+    {
+        v.push_back(x);
+    }
+    int pop()
+    {
+        int res=v.back();
+        v.pop_back();
+        return res;
+    }
+    int peek()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+int main()
+{
+   Stack s;
+    s.push(5);
+    s.push(10);
+    s.push(20);
+    cout<<s.pop()<<endl;
+    cout<<s.size()<<endl;
+    cout<<s.peek()<<endl;
+    cout<<s.empty()<<endl;
+
+    return 0;
+}
