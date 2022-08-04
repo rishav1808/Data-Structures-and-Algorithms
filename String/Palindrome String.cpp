995aab72102fbd22f920954edcb65b360571ea0c
rishav1808 <rishavraj1808@gmail.com>
2022-08-04 19:55:11 +0530

    Added Palindrome String

---
+++ String/Palindrome String.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	   stack<char>s;
+	   for(auto x :S)
+	   s.push(x);
+	   string rev="";
+	   while(!s.empty())
+	  {
+	      rev+=s.top();
+	      s.pop();
+	  }
+	  if(S==rev)
+	  return 1;
+	  return 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
